4aa72ca2eaf521c44da2f70bf6494047498fef7f
csaba012 <papp.csaba28@gmail.com>
2024-06-10 17:41:27 +0200

    Lesson_17

--- Lesson_15-17/Lesson_15-17_h.py
+++ Lesson_15-17/Lesson_15-17_h.py
@@ -1,5 +1,7 @@
 import pygame
 import os
+import random
+import math
 
 pygame.font.init()
 pygame.mixer.init()
@@ -28,6 +30,20 @@ LEZER_SEB = 7
 MAX_LOVEDEK = 3
 URHAJO_SZELESSEG = 80
 URHAJO_MAGASSAG = 60
+METEOR_SZELESSEG = 50
+METEOR_MAGASSAG = 50
+METEOR_SEB = 2
+
+METEOR_1_IRANY = random.randint(0, 359)
+METEOR_2_IRANY = random.randint(0, 359)
+METEOR_3_IRANY = random.randint(0, 359)
+
+METEOR_1_X_SEB = math.cos(METEOR_1_IRANY) * METEOR_SEB
+METEOR_1_Y_SEB = math.sin(METEOR_1_IRANY) * METEOR_SEB
+METEOR_2_X_SEB = math.cos(METEOR_2_IRANY) * METEOR_SEB
+METEOR_2_Y_SEB = math.sin(METEOR_2_IRANY) * METEOR_SEB
+METEOR_3_X_SEB = math.cos(METEOR_3_IRANY) * METEOR_SEB
+METEOR_3_Y_SEB = math.sin(METEOR_3_IRANY) * METEOR_SEB
 
 SARGA_TALALAT = pygame.USEREVENT + 1
 PIROS_TALALAT = pygame.USEREVENT + 2
@@ -42,7 +58,16 @@ PIROS_URHAJO = pygame.transform.rotate(
 
 HATTER = pygame.transform.scale(pygame.image.load(os.path.join('Lesson_15-17', 'assets', 'space.png')), (SZELESSEG, MAGASSAG))
 
-def ablak_rajzol(piros, sarga, piros_lovedekek, sarga_lovedekek, piros_elet, sarga_elet):
+METEOR_KEP = pygame.image.load(os.path.join('Lesson_15-17', 'assets', 'meteor.png'))
+METEOR_1 = pygame.transform.rotate(
+    pygame.transform.scale(METEOR_KEP, (METEOR_SZELESSEG, METEOR_MAGASSAG)), 90)
+METEOR_2 = pygame.transform.rotate(
+    pygame.transform.scale(METEOR_KEP, (METEOR_SZELESSEG, METEOR_MAGASSAG)), 90)
+METEOR_3 = pygame.transform.rotate(
+    pygame.transform.scale(METEOR_KEP, (METEOR_SZELESSEG, METEOR_MAGASSAG)), 90)
+
+def ablak_rajzol(piros, sarga, piros_lovedekek, sarga_lovedekek, 
+                 piros_elet, sarga_elet, meteor_1, meteor_2, meteor_3):
     #ABLAK.fill(FEHER)
     ABLAK.blit(HATTER, (0, 0))
     pygame.draw.rect(ABLAK, FEKETE, ELVALASZTO)
@@ -54,6 +79,9 @@ def ablak_rajzol(piros, sarga, piros_lovedekek, sarga_lovedekek, piros_elet, sar
     
     ABLAK.blit(SARGA_URHAJO, (sarga.x, sarga.y))
     ABLAK.blit(PIROS_URHAJO, (piros.x, piros.y))
+    ABLAK.blit(METEOR_1, (meteor_1.x, meteor_1.y))
+    ABLAK.blit(METEOR_2, (meteor_2.x, meteor_2.y))
+    ABLAK.blit(METEOR_3, (meteor_3.x, meteor_3.y))
 
     for lovedek in piros_lovedekek:
         pygame.draw.rect(ABLAK, PIROS, lovedek)
@@ -81,13 +109,17 @@ def piros_mozgat(gombok_lenyomva, piros):
     if gombok_lenyomva[pygame.K_DOWN] and piros.y + SEB + piros.height < MAGASSAG: #le
         piros.y += SEB
 
-def lovedek_mozgat(sarga_lovedekek, piros_lovedekek, sarga, piros):
+def lovedek_mozgat(sarga_lovedekek, piros_lovedekek, sarga, piros, 
+                   meteor_1, meteor_2, meteor_3):
     for lovedek in sarga_lovedekek:
         lovedek.x += LEZER_SEB
         if piros.colliderect(lovedek):
             pygame.event.post(pygame.event.Event(PIROS_TALALAT))
             sarga_lovedekek.remove(lovedek)
         elif lovedek.x > SZELESSEG: 
+            sarga_lovedekek.remove(lovedek)
+        elif (meteor_1.colliderect(lovedek) or meteor_2.colliderect(lovedek) 
+              or meteor_3.colliderect(lovedek)):
             sarga_lovedekek.remove(lovedek)
     for lovedek in piros_lovedekek:
         lovedek.x -= LEZER_SEB
@@ -96,6 +128,69 @@ def lovedek_mozgat(sarga_lovedekek, piros_lovedekek, sarga, piros):
             piros_lovedekek.remove(lovedek)
         elif lovedek.x < 0:
             piros_lovedekek.remove(lovedek)
+        elif (meteor_1.colliderect(lovedek) or meteor_2.colliderect(lovedek) 
+              or meteor_3.colliderect(lovedek)):
+            piros_lovedekek.remove(lovedek)
+    if sarga.colliderect(meteor_1):
+        meteor_1.x = 480
+        meteor_1.y = 240
+        pygame.event.post(pygame.event.Event(SARGA_TALALAT))
+    if sarga.colliderect(meteor_2):
+        meteor_2.x = 480
+        meteor_2.y = 240
+        pygame.event.post(pygame.event.Event(SARGA_TALALAT))
+    if sarga.colliderect(meteor_3):
+        meteor_3.x = 480
+        meteor_3.y = 240
+        pygame.event.post(pygame.event.Event(SARGA_TALALAT))
+
+    if piros.colliderect(meteor_1):
+        meteor_1.x = 480
+        meteor_1.y = 240
+        pygame.event.post(pygame.event.Event(PIROS_TALALAT))
+    if piros.colliderect(meteor_2):
+        meteor_2.x = 480
+        meteor_2.y = 240
+        pygame.event.post(pygame.event.Event(PIROS_TALALAT))
+    if piros.colliderect(meteor_3):
+        meteor_3.x = 480
+        meteor_3.y = 240
+        pygame.event.post(pygame.event.Event(PIROS_TALALAT))
+
+def meteor_mozgat(meteor_1, meteor_2, meteor_3):
+    meteor_1.x += METEOR_1_X_SEB
+    meteor_1.y += METEOR_1_Y_SEB 
+    meteor_2.x += METEOR_2_X_SEB
+    meteor_2.y += METEOR_2_Y_SEB 
+    meteor_3.x += METEOR_3_X_SEB
+    meteor_3.y += METEOR_3_Y_SEB 
+
+    if meteor_1.x > 999:
+        meteor_1.x = 1
+    if meteor_1.x < 1:
+        meteor_1.x = 999
+    if meteor_1.y > 499:
+        meteor_1.y = 1
+    if meteor_1.y < 1: 
+        meteor_1.y = 499
+
+    if meteor_2.x > 999:
+        meteor_2.x = 1
+    if meteor_2.x < 1:
+        meteor_2.x = 999
+    if meteor_2.y > 499:
+        meteor_2.y = 1
+    if meteor_2.y < 1: 
+        meteor_2.y = 499
+
+    if meteor_3.x > 999:
+        meteor_3.x = 1
+    if meteor_3.x < 1:
+        meteor_3.x = 999
+    if meteor_3.y > 499:
+        meteor_3.y = 1
+    if meteor_3.y < 1: 
+        meteor_3.y = 499
 
 def nyertes_rajzol(nyertes_szoveg):
     szoveg = WINNER_BETU.render(nyertes_szoveg, True, FEHER)
@@ -113,6 +208,10 @@ def main():
 
     piros_elet = 10
     sarga_elet = 10
+
+    meteor_1 = pygame.Rect(480, 240, METEOR_SZELESSEG, METEOR_MAGASSAG)
+    meteor_2 = pygame.Rect(480, 240, METEOR_SZELESSEG, METEOR_MAGASSAG)
+    meteor_3 = pygame.Rect(480, 240, METEOR_SZELESSEG, METEOR_MAGASSAG)
 
     ora = pygame.time.Clock()
     game = True
@@ -148,8 +247,11 @@ def main():
         gombok_lenyomva = pygame.key.get_pressed()
         sarga_mozgat(gombok_lenyomva, sarga)
         piros_mozgat(gombok_lenyomva, piros)
-        lovedek_mozgat(sarga_lovedekek, piros_lovedekek, sarga, piros)
-        ablak_rajzol(piros, sarga, piros_lovedekek, sarga_lovedekek, piros_elet, sarga_elet)
+        meteor_mozgat(meteor_1, meteor_2, meteor_3)
+        lovedek_mozgat(sarga_lovedekek, piros_lovedekek, 
+                       sarga, piros, meteor_1, meteor_2, meteor_3)
+        ablak_rajzol(piros, sarga, piros_lovedekek, sarga_lovedekek, 
+                     piros_elet, sarga_elet, meteor_1, meteor_2, meteor_3)
     pygame.quit()
 
 
